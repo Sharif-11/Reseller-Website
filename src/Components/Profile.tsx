ea435d1f63ac5a1a2b3001c9b81d388c59acf266
Sharif-11 <sharifsharifulislam061@gmail.com>
2025-02-05 16:46:30 +0600

    profile enlarged

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -72,8 +72,8 @@ const Profile = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4 my-16">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4 my-3">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
         <h1 className="text-xl font-bold text-[#87594e] text-center mb-4">
           প্রোফাইল আপডেট করুন
         </h1>
